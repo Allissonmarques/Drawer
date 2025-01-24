72378cb673f2b8c1ebf04f20cf5343f30e442dce
Allissonmarques <allissonmarques00@gmail.com>
2025-01-24 08:34:26 -0300

    Atualização do projeto sorteio.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
 function generateNumber(){
     const inputMin = Math.ceil(document.querySelector(".input-min").value)
     const inputMax = Math.floor(document.querySelector(".input-max").value)
-    const result = Math.floor(Math.random() * (inputMax - inputMin + 1) + inputMin)
     
-    alert(result)
+    if(inputMin >= inputMax ){
+        alert("O valor mínimo tem que ser MENOR que o valor máximo.")
+    }
+
+    else{
+        const result = Math.floor(Math.random() * (inputMax - inputMin + 1) + inputMin)
+        alert(result)
+    }
+    
+    
 }
